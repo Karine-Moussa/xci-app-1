eef5582acf38718f5fc3b15cb81b71744b0d90e3
Karine Moussa <yehkarine@gmail.com>
2020-12-08 10:42:31 -0500

    cleaned up directories, removed 'functions' directory

--- functions/format_input_data.R
+++
@@ -1,4 +0,0 @@
-# Import data needed
-x_expr <- read.delim("data_sources/x_expr.tsv",header=T,sep="\t",na.strings="?")
-xchrom_map_colored <- read.delim("data_intermediate/xchrom_map_colored",header=T,sep="\t")
-gene_stat_table <- read.csv("data_intermediate/gene_stat_table.csv",header=T)
